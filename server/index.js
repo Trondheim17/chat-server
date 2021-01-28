4fd820725b7b35ebdda6cdcdbc29158f80705ce8
Austin Reece <austin.m.reece@gmail.com>
2021-01-28 16:28:30 -0700

    Solution

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mc = require('./controllers/message_controller')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static(__dirname + '/../public/build'))
+
+const messageBaseUrl = '/api/messages'
+app.post(messageBaseUrl, mc.create)
+app.get(messageBaseUrl, mc.read)
+app.put(`${messageBaseUrl}/:id`, mc.update)
+app.delete(`${messageBaseUrl}/:id`, mc.delete)
+
+const port = 3001
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`)
+})
